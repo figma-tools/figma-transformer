554d4e366e3b4067fcffa5b9045b422d4e980f7a
Sławomir Kołodziej <slawomir.kolodziej@netguru.pl>
2020-01-11 19:12:08 +0100

    Change order of exports

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,9 @@ import { Node, NodeType } from "figma-js";
 
 export type Shortcut = NodeType | "STYLE";
 
-export type Shortcuts = Record<Shortcut, TransformedNode[]>;
-
 export type TransformedNode = Node & { shortcuts?: Shortcuts };
+
+export type Shortcuts = Record<Shortcut, TransformedNode[]>;
 
 export type ProcessedFile = {
     fileId: string;
