cfda85e4ce346c571deb89b8acc3c80c0055bc8e
Bernardo Raposo <hi@bernardoraposo.com>
2020-06-28 23:39:16 +0100

    Improve shortcut types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,51 @@
-import { Node, NodeType } from "figma-js";
+import {
+    Node,
+    NodeType,
+    Component,
+    Canvas,
+    Instance,
+    Line,
+    Frame,
+    Group,
+    Vector,
+    Star,
+    Ellipse,
+    BooleanGroup,
+    RegularPolygon,
+    Rectangle,
+    Text,
+    Slice,
+    Style,
+    ComponentMetadata,
+    Paint,
+    TypeStyle,
+} from "figma-js";
 
-export type Shortcut = NodeType | "STYLE";
+export type ShortcutType = NodeType | "STYLE";
 
-export type TransformedNode = Node & { shortcuts?: Shortcuts };
-
-export type Shortcuts = Record<Shortcut, TransformedNode[]>;
+export type NodeWithShortcuts<T extends Node> = T & { shortcuts?: Shortcuts };
+export type StyleNode = Style & Node & { styles: Paint; textStyles: TypeStyle };
+export type Shortcuts = Record<
+    "components",
+    NodeWithShortcuts<Component & ComponentMetadata>[]
+> &
+    Record<"pages", NodeWithShortcuts<Canvas>[]> &
+    Record<"lines", NodeWithShortcuts<Line>[]> &
+    Record<"instances", NodeWithShortcuts<Instance>[]> &
+    Record<"frames", NodeWithShortcuts<Frame>[]> &
+    Record<"groups", NodeWithShortcuts<Group>[]> &
+    Record<"vectors", NodeWithShortcuts<Vector>[]> &
+    Record<"booleans", NodeWithShortcuts<BooleanGroup>[]> &
+    Record<"stars", NodeWithShortcuts<Star>[]> &
+    Record<"ellipses", NodeWithShortcuts<Ellipse>[]> &
+    Record<"regularPolygons", NodeWithShortcuts<RegularPolygon>[]> &
+    Record<"rectangles", NodeWithShortcuts<Rectangle>[]> &
+    Record<"texts", NodeWithShortcuts<Text>[]> &
+    Record<"slices", NodeWithShortcuts<Slice>[]> &
+    Record<"styles", StyleNode[]>;
 
 export type ProcessedFile = {
-    fileId: string;
+    fileId?: string;
     name: string;
     lastModified: string;
     thumbnailUrl: string;
